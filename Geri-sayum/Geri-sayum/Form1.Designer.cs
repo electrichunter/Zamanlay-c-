1f0305e4b97395dc33c5f1361ebefde3c03e1235
Ömer Fruk Uysal <113265992+electrichunter@users.noreply.github.com>
2024-04-25 19:44:37 +0300

    basit bir geri sayım app

---
+++ Geri-sayum/Geri-sayum/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Geri_sayum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(104, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(223, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kalan zaman";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(212, 28);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(211, 24);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 369);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+    }
+}
+
---
+++ Geri-sayum/Geri-sayum/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using static System.Net.Mime.MediaTypeNames;
+using System.Media;
+namespace Geri_sayum
+
+{
+    public partial class Form1 : Form
+    {
+
+        private System.Windows.Forms.Timer countdownTimer;
+        private DateTime targetDateTime;
+        private System.Timers.Timer coundtdowntimer; // System.Timers.Timer olarak değiştirin
+        private System.Timers.Timer musicStopTimer; // 20 saniye sonra müziği durdurmak için zamanlayıcı
+        public Form1()
+        {
+            InitializeComponent();
+            // Tarih ve saat seçimi için DateTimePicker ayarlama
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss"; // Tarih ve saat formatı
+            dateTimePicker1.ShowUpDown = true; // Saat seçimi için yukarı/aşağı düğmeleri
+
+            // countdownTimer'ı başlatın ve olay işleyicisini bağlayın
+            countdownTimer = new System.Windows.Forms.Timer();
+            countdownTimer.Interval = 1000; // 1 saniye aralıklarla
+            countdownTimer.Tick += OnCountdownTimerElapsed; // Olay işleyicisini bağla
+
+            // 20 saniye sonra müziği durdurmak için zamanlayıcı
+            musicStopTimer = new System.Timers.Timer();
+            musicStopTimer.Interval = 20000; // 20 saniye
+            musicStopTimer.Elapsed += StopMusic; // Zamanlayıcı olayı için işlemci ekleyin
+
+            // NotifyIcon ayarları
+            notifyIcon1.Text = "Geri Sayım Uygulaması";
+            notifyIcon1.Icon = new Icon("C:\\Users\\ömer\\Downloads\\hourglass_23777.ico");
+            notifyIcon1.Visible = true;
+
+            // Bağlam menüsü
+            ContextMenuStrip contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Geri Sayımı Başlat", null, StartCountdown);
+            contextMenu.Items.Add("Kapat", null, CloseApplication);
+            notifyIcon1.ContextMenuStrip = contextMenu;
+
+            // Diğer olaylar için ayarlamalar
+            this.Resize += new EventHandler(Form1_Resize);
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+        }
+        private void StartCountdown(object sender, EventArgs e)
+        {
+            if (countdownTimer != null)
+            {
+                targetDateTime = dateTimePicker1.Value; // Hedef tarih ve saati alın
+                countdownTimer.Start(); // Geri sayımı başlat
+            }
+        }
+       
+        private void CloseApplication(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit(); // Uygulamayı kapat
+        }
+        private void OnCountdownTimerElapsed(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan timeRemaining = targetDateTime - now;
+
+            if (timeRemaining.TotalSeconds <= 0)
+            {
+                countdownTimer.Stop();
+                UpdateLabel("Süre Doldu!");
+
+                PlaySound("C:\\Users\\ömer\\Downloads\\mp3indirdur-Tarkan-Son-Durak.wav");
+            }
+            else
+            {
+                UpdateLabel($"Gün: {timeRemaining.Days}, Saat: {timeRemaining.Hours}, Dakika: {timeRemaining.Minutes}, Saniye: {timeRemaining.Seconds}");
+            }
+        }
+
+        private void PlaySound(string soundFilePath)
+        {
+            SoundPlayer player = new SoundPlayer(soundFilePath);
+            player.Play(); // Asenkron olarak çal
+            musicStopTimer.Start();
+        }
+
+        private void StopMusic(object sender, ElapsedEventArgs e)
+        {
+            musicStopTimer.Stop(); // Zamanlayıcıyı durdur
+            // Müzik çalmayı durdurmak için SoundPlayer kullanarak müziği tekrar başlatın (bu, SoundPlayer'da tek yol)
+            SoundPlayer player = new SoundPlayer();
+            player.Stop(); // Ses çalmayı durdur
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (countdownTimer != null) // Null kontrolü
+            {
+                StartCountdown(sender, e); // Geri sayımı başlat
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            targetDateTime = dateTimePicker1.Value; // Seçilen tarihi güncelleyin
+            DateTime now = DateTime.Now;
+            TimeSpan timeRemaining = targetDateTime - now;
+
+            if (timeRemaining.TotalSeconds <= 0)
+            {
+                countdownTimer.Stop();
+                UpdateLabel("Süre Doldu!");
+            }
+            else
+            {
+                UpdateLabel($"Gün: {timeRemaining.Days}, Saat: {timeRemaining.Hours}, Dakika: {timeRemaining.Minutes}, Saniye: {timeRemaining.Seconds}");
+            }
+
+        }
+
+        private void UpdateLabel(string text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => label1.Text = text));
+            }
+            else
+            {
+                label1.Text = text;
+            }
+        }
+        private void label1_Click(string text)
+        {
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide(); // Formu gizle
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Uygulama kapatılmak istendiğinde simge durumuna getirin
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                this.Hide(); // Formu gizle
+                e.Cancel = true; // Kapama olayını iptal et
+            }
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // Formu tekrar göstermek için kod
+            this.Show();
+            this.WindowState = FormWindowState.Normal; // Normal duruma getir
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
